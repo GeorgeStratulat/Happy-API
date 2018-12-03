8ef871b04618fed6c08929573be4521a5c9af0d4
Andrei Clejanu <andreiclejanu@Andreis-MacBook-Pro.local>
2018-12-03 18:56:42 +0200

    another on23e

--- routes/users.js
+++ routes/users.js
@@ -41,7 +41,7 @@ router.post("/login", (req,res) =>{
         parola: req.body.parola
     });
     Users.findOne({"email": user.email}).exec().then(doc=>{
-       res.send({"success":true, "message": user._id});
+       res.send({"success":true, "message": doc._id});
       })
       .catch(err => {
         console.log(err);
