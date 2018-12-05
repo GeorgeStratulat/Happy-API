b00e0559dd4807cc8a311bf351b37c2e119d9315
Andrei Clejanu <andreiclejanu@Andreis-MacBook-Pro.local>
2018-12-05 15:03:07 +0200

    fdsfds

--- routes/abonamente.js
+++ routes/abonamente.js
@@ -81,6 +81,27 @@ router.patch("/:abonamentId", (req,res)=>{
     });
 });
 
+router.patch("/:abonamentId/nume_abonament", (req,res)=>{
+    const id = req.params.abonamentId;
+    Abonamente.update({_id:id}, {$set:{
+        nume_abonament: req.body.nume_abonament
+    }
+    })
+    .exec()
+    .then(result => {
+      console.log(result);
+      res.status(200).json({
+        message: "Employee Updated!"
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
+});
+
 router.delete("/:abonamentId", (req,res) =>{
     const id = req.params.abonamentId;
     Abonamente.remove({ _id: id })
