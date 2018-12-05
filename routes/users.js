b3256e6570659f30ffb198974a11648c3172ea6e
Andrei Clejanu <andreiclejanu@Andreis-MacBook-Pro.local>
2018-12-05 14:36:05 +0200

    come on

--- routes/users.js
+++ routes/users.js
@@ -144,6 +144,27 @@ router.patch("/:userId", (req,res)=>{
     });
 });
 
+router.patch("/:userId/tip_abonament", (req,res)=>{
+    const id = req.params.userId;
+    Users.update({_id:id}, {$set:{
+        tip_abonament: req.body.tip_abonament,
+    }
+    })
+    .exec()
+    .then(result => {
+      console.log(result);
+      res.status(200).json({
+        message: "Employee Updated!"
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
+});
+
 router.delete("/:userId", (req,res) =>{
     const id = req.params.userId;
     Users.remove({ _id: id })
