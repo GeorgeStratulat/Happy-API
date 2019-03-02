31e5cb16b8071c45822b447329b4af042acb2763
Andrei Clejanu <doruandrei1998@yahoo.com>
2019-03-02 19:48:40 +0200

    popopopowrowow

--- routes/admin_venue.js
+++ routes/admin_venue.js
@@ -18,7 +18,7 @@ router.get("/", (req,res)=>{
 router.post("/login", (req,res) =>{
     const admin_venue = new Admin_Venues({
         username: req.body.username,
-        password: req.body.password
+        password: req.body.password,
     });
     Admin_Venues.findOne({"username": admin_venue.username, "password": admin_venue.password}).exec().then(doc=>{
         
@@ -30,6 +30,29 @@ router.post("/login", (req,res) =>{
         res.status(404).json({ message: "Can't find shit"});
       });
     ;
+});
+
+router.post("/addUser", (req,res) =>{
+    const user = new Admin_Venues({
+        _id : mongoose.Types.ObjectId(),
+        venue_id: req.body.venue_id,
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    user.save().then(result=>{
+        console.log(result);
+        res.status(201).json({
+            message:"User salvat!",
+            UserAdaugat: user
+        });
+    })
+    .catch(err=>{
+        console.log(err);
+        res.status(500).json({
+            error:err
+        });
+    });
 });
 
 module.exports = router;
